3afb31a9f8d79bc2c60d8b1659a97e7ac8c81021
Milad Haghighi <miladhaghighi@Milads-MacBook-Air.local>
2024-08-05 22:12:47 -0700

    first commit

---
+++ learningjs.js
@@ -0,0 +1,22 @@
+document.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    const anchor = document.getElementById('anchor')
+    const rekt = anchor.getBoundingClientRect();
+    const anchorx = rekt.left + rekt.width / 2;
+    const anchory = rekt.top + rekt.height / 2;
+    const angleDeg = angle(mouseX, mouseY, anchorx, anchory)
+    console.log(angleDeg)
+    const eyes = document.querySelectorAll('.eye')
+    eyes.forEach(eye => {
+        eye.style.transform = `rotate(${90+ angleDeg}deg)`;
+    })
+})
+
+function angle(cx, cy, ex, ey){
+    const dy = ey - cy;
+    const dx = ex - cx;
+    const rad = Math.atan2(dy, dx)
+    const deg = rad * 180 / Math.PI;
+    return deg
+}
