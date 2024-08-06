6108808e5df65ce43fe1cc4d2f10cb125a684fd3
Milad Haghighi <miladhaghighi@Milads-MacBook-Air.local>
2024-08-06 11:56:50 -0700

    eyes track individually

--- learningjs.js
+++ learningjs.js
@@ -2,14 +2,19 @@ document.addEventListener('mousemove', (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
     const anchor = document.getElementById('anchor')
-    const rekt = anchor.getBoundingClientRect();
-    const anchorx = rekt.left + rekt.width / 2;
-    const anchory = rekt.top + rekt.height / 2;
-    const angleDeg = angle(mouseX, mouseY, anchorx, anchory)
-    console.log(angleDeg)
+    const rekt1 = eye1.getBoundingClientRect();
+    const rekt2 = eye2.getBoundingClientRect();
+    const anchorx1 = rekt1.left + rekt1.width / 2;
+    const anchory1 = rekt1.top + rekt1.height / 2;
+    const angleDeg1 = angle(mouseX, mouseY, anchorx1, anchory1)
+    const anchorx2 = rekt2.left + rekt2.width / 2;
+    const anchory2 = rekt2.top + rekt2.height / 2;
+    const angleDeg2 = angle(mouseX, mouseY, anchorx2, anchory2)
+    console.log(angleDeg1)
     const eyes = document.querySelectorAll('.eye')
     eyes.forEach(eye => {
-        eye.style.transform = `rotate(${90+ angleDeg}deg)`;
+        eye1.style.transform = `rotate(${90+ angleDeg1}deg)`;
+        eye2.style.transform = `rotate(${90+ angleDeg2}deg)`;
     })
 })
 
